4dc94cbc95cfb432ca6d75e675987cdba39189f7
frenude <frenude@gmail.com>
2021-02-19 11:16:48 +0800

    docker-install.sh

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# shell
+# 一些用来服务器部署的Shell脚本
+## 使用步骤
+-   修改`Shell`的权限` chmod +x fileName.sh`
+-   执行脚本`./fileName.sh`
+
+### 目录
+
+-   [`docker-install`](docker-install/docker-install.sh) docker的下载与安装
---
+++ docker-install/docker-install.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env sh
+
+# 卸载旧版本
+sudo yum remove docker docker-client docker-client-latest docker-common docker-latest docker-latest-logrotate docker-logrotate docker-engine
+
+# 安装 Docker Engine-Community 依赖
+sudo yum install -y yum-utils device-mapper-persistent-data lvm2
+
+# 使用官方源地址
+# sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+
+# 使用阿里云镜像源
+sudo yum-config-manager --add-repo http://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo
+
+# 安装 Docker Engine-Community
+sudo yum install -y docker-ce docker-ce-cli containerd.io
+
+# 启动 Docker。
+sudo systemctl start docker
+
+# 通过运行 hello-world 映像来验证是否正确安装了 Docker Engine-Community 。
+sudo docker run hello-world
